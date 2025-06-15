d0fb8ab226e5e136e27f106e0caf02914b3aed14
abdulbosit <abdulbositabdurahimov75@gmail.com>
2025-06-15 09:49:03 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/backend/olxapp/OlxAppApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.backend.olxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlxAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/backend/olxapp/OlxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.backend.olxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlxAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
