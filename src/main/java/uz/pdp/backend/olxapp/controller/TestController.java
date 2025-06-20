a5fe315fe666d57ba719f5392e121e8c92c835fc
Avazbek712 <avazbekloz123@gmail.com>
2025-06-20 22:34:02 +0500

    Minor

--- src/main/java/uz/pdp/backend/olxapp/controller/TestController.java
+++ src/main/java/uz/pdp/backend/olxapp/controller/TestController.java
@@ -1,12 +1,8 @@
 package uz.pdp.backend.olxapp.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uz.pdp.backend.olxapp.entity.Attachment;
-import uz.pdp.backend.olxapp.mapper.AttachmentMapper;
-import uz.pdp.backend.olxapp.payload.AttachmentDTO;
 
 /**
  * Created by Avazbek on 18/06/25 17:22
@@ -16,18 +12,4 @@ import uz.pdp.backend.olxapp.payload.AttachmentDTO;
 @RequiredArgsConstructor
 public class TestController {
 
-    private final AttachmentMapper attachmentMapper;
-
-
-    @GetMapping
-    public AttachmentDTO getAttachment() {
-        Attachment attachment = new Attachment();
-
-        attachment.setContentType("text/plain");
-        attachment.setOriginalName("test.txt");
-        attachment.setFileSize(34L);
-        attachment.setPath("C:\\Users\\Avazbek\\Desktop\\test.txt");
-        return attachmentMapper.toDto(attachment);
-
-    }
 }
