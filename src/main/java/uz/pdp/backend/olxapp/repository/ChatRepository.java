f5e86e29909044a7d222af2e8c3e86792a1fd0eb
Avazbek712 <avazbekloz123@gmail.com>
2025-06-23 22:39:30 +0500

    Added query based methods

--- src/main/java/uz/pdp/backend/olxapp/repository/ChatRepository.java
+++ src/main/java/uz/pdp/backend/olxapp/repository/ChatRepository.java
@@ -1,7 +1,20 @@
 package uz.pdp.backend.olxapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.pdp.backend.olxapp.entity.Chat;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+
+
+    @Query("SELECT c FROM chats c WHERE c.product.id = :advertisementId " +
+            "AND ((c.userOne.id = :userOneId AND c.userTwo.id = :userTwoId) " +
+            "OR (c.userOne.id = :userTwoId AND c.userTwo.id = :userOneId))")
+    Optional<Chat> findChatByUsersAndProduct(Long userOneId, Long userTwoId, Long advertisementId);
+
+    @Query("SELECT c FROM chats c WHERE c.userOne.id = :userId OR c.userTwo.id = :userId")
+    List<Chat> findAllByUserId(Long userId);
 }
