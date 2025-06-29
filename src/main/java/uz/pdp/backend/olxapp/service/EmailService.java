ae8f5ef99bb9c438bf13f423066b4850b2fd7a86
Avazbek712 <avazbekloz123@gmail.com>
2025-06-30 00:28:49 +0500

    Minor

--- src/main/java/uz/pdp/backend/olxapp/service/EmailService.java
+++ src/main/java/uz/pdp/backend/olxapp/service/EmailService.java
@@ -1,6 +1,7 @@
 package uz.pdp.backend.olxapp.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
